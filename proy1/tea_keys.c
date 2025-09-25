e8a216708bc0b6a88ec57ca7dacfb9c08d1018ad
Jose Herrera <josexd96@gmail.com>
2025-09-25 12:32:59 -0600

    Update tea_keys.c

--- proy1/tea_keys.c
+++ proy1/tea_keys.c
@@ -5,12 +5,16 @@
 
 void sel_key(uint32_t key[4]);
 void sel_key(uint32_t key[4]){
+    char buffer[20];
     for (size_t i = 0; i < 4; i++)
     {
         printf("Enter new key part %zu (hex): ", i+1);
         fflush(stdout);
         if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
             key[i] = strtoul(buffer, NULL, 16);
+        } else {
+            fprintf(stderr, "Error reading key part %zu\n", i+1);
+            exit(EXIT_FAILURE);
         }
     }
 }
